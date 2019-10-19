3d4e85c6e0620f7d5ecdd010db3fa014fe8884ce
Pramod-0215 <pramodray0215@gmail.com>
2019-10-19 18:37:02 +0530

    The hotelmanagement project with validation

--- hrms/hrmsapp/admin.py
+++ hrms/hrmsapp/admin.py
@@ -1,32 +1,103 @@
 from django.contrib import admin
-from  .models import Hotel,RoomType,Room,Guest,Manager,Record,Booking
+from django import forms
+from  .models import Hotel,RoomType,Room,Guest,Manager,Record
+
+class HotelAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(HotelAdminForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        author = self.cleaned_data.get('hotel_name')
+        if len(author) < 4:
+            raise forms.ValidationError("Hotel name cannot be less than 4", code='error')
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        return super(HotelAdminForm, self).save(commit=commit)
+
+class RoomAdminFrom(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RoomAdminFrom,self).__init__(*args, **kwargs)
+
+
+    def clean(self):
+        guest_number = self.cleaned_data.get('bed_number')
+        room_number1 = self.cleaned_data.get('room')
+        if guest_number > 3:
+            raise forms.ValidationError('In one room max 3 person only allowed',code='error')
+
+
+        return self.cleaned_data
+
+    def save(self,commit= True):
+        return super(RoomAdminFrom,self).save(commit=commit)
+
+class RecordAdminFrom(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RecordAdminFrom,self).__init__(*args, **kwargs)
+
+
+    def clean(self):
+        checked_out_date = self.cleaned_data.get('checkout_date')
+        checked_in_date = self.cleaned_data.get('checkin_date')
+        booking_date1 = self.cleaned_data.get('booking_date')
+        rooms = self.cleaned_data.get('room')
+
+
+        # if checked_out_date==None:
+        #     raise forms.ValidationError('Checkout date invalid!', code='error')
+
+        #Chech in date has to be greater than today's date
+        #Check out date has to be greater than check in date
+
+        if checked_in_date > checked_out_date:
+            raise forms.ValidationError('Checkout date invalid!',code='error')
+        if booking_date1> checked_out_date:
+            raise forms.ValidationError('Checkout date invalid!', code='error')
+        if booking_date1> checked_in_date:
+            raise forms.ValidationError('Checkout date invalid!', code='error')
+
+
+
+        if Record.objects.filter(room=rooms).exists():
+            if Record.objects.filter(checkin_date=checked_in_date).exists():
+                if Record.objects.filter(checkout_date=checked_out_date).exists():
+
+                    raise forms.ValidationError("Can't booked this room , this is already booked", code='error')
+        return self.cleaned_data
+
+
+    def save(self,commit= True):
+        return super(RecordAdminFrom,self).save(commit=commit)
 
 class HotelAdmin(admin.ModelAdmin):
     list_display = ('hotel_name', 'hotel_location',)
+    form = HotelAdminForm
 admin.site.register(Hotel,HotelAdmin)
 
 class RoomTypeAdmin(admin.ModelAdmin):
     list_display = ('name','room_rent',)
+
 admin.site.register(RoomType,RoomTypeAdmin)
 
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('room_number','room_type','availability','bed_number',)
+    form = RoomAdminFrom
 admin.site.register(Room,RoomAdmin)
+
 
 class GuestAdmin(admin.ModelAdmin):
     list_display = ('guest_name',)
 admin.site.register(Guest,GuestAdmin)
 
-class ManagerAdmin(admin.ModelAdmin):
-    list_display = ('manager_name',)
-admin.site.register(Manager,ManagerAdmin)
 
-# class BookingAdmin(admin.ModelAdmin):
-#     list_display = ('hotel_name','room1','is_avilable','cancel',)
-# admin.site.register(Booking,BookingAdmin)
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ('manager_name','manager_phone','manager_email')
+admin.site.register(Manager,ManagerAdmin)
 
 
 class RecordAdmin(admin.ModelAdmin):
-    list_display = ('guest','checkin_date','checkout_date','booking_date','checked_in','manager','cancel','bill')
+    list_display = ('guest','checkin_date','checkout_date','checked_in','manager','cancel','bill')
+    form = RecordAdminFrom
 admin.site.register(Record,RecordAdmin)
 # Register your models here.
--- hrms/hrmsapp/models.py
+++ hrms/hrmsapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db import models
-import datetime
+
 # Create your models here.
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -24,7 +24,7 @@ class RoomType(models.Model):
 class Room(models.Model):
     room_number = models.IntegerField(default=101)
     room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
-    availability = models.BooleanField(default=True)
+    availability = models.BooleanField(default=False)
     bed_number = models.IntegerField(default=2)
 
     def __str__(self):
@@ -44,37 +44,46 @@ class Manager(models.Model):
     manager_name = models.CharField(max_length=100)
     manager_phone = models.IntegerField()
     manager_email = models.EmailField()
-    # # hotel_location = models.ForeignKey(Hotel,on_delete=models.CASCADE)
+    # hotel_location = models.ForeignKey(Hotel,on_delete=models.CASCADE)
 
 
     def __str__(self):
         return self.manager_name
 
-# class Booking(models.Model):
-#     hotel_name = models.ForeignKey(Hotel,on_delete=models.CASCADE)
-#     room1 = models.ForeignKey(Room,on_delete=models.CASCADE)
-#     is_avilable = models.BooleanField(default=True)
-#     cancel = models.BooleanField(default=False)
-#     booking_date = models.DateTimeField()
-#     checkin_date = models.DateTimeField()
-#     checkout_date = models.DateTimeField()
+
 
 class Record(models.Model):
     guest = models.ForeignKey(Guest,on_delete=models.CASCADE)
-    booking_date = models.DateTimeField()
-    checkin_date = models.DateTimeField()
-    checkout_date = models.DateTimeField()
+    booking_date = models.DateField()
+    checkin_date = models.DateField()
+    checkout_date = models.DateField()
     room = models.ForeignKey(Room,on_delete=models.CASCADE)
     manager = models.ForeignKey(Manager,on_delete=models.CASCADE)
     checked_in = models.BooleanField(default=False)
     cancel = models.BooleanField(default=False)
-    # total_cost = models.IntegerField()
+
 
     def __str__(self):
         return str(self.booking_date) + self.guest.guest_name + self.manager.manager_name
 
 
     def bill(self):
-        day = (self.checkout_date - self.checkin_date).days
+        day = 1
         rent = self.room.room_type.room_rent
+        if (self.checkout_date - self.checkin_date).days == 0:
+            return day*rent
+        else:
+            day= (self.checkout_date - self.checkin_date).days
         return day*rent
+
+@receiver(post_save, sender=Record)
+def borrowed(sender, instance, **kwargs):
+    #if checked_in and if it is not cancelled then make that room not available else it is available
+
+
+    if instance.cancel :
+        instance.room.availability = True
+    else:
+        instance.room.availability = False
+    instance.room.save()
+
